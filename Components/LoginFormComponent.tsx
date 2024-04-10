ac81f97b587968533c8ccfb09494a8afbcba4a36
et120 <e.trotter120@gmail.com>
2024-04-10 10:59:50 -0700

    login navigation add

--- Components/LoginFormComponent.tsx
+++ Components/LoginFormComponent.tsx
@@ -1,14 +1,26 @@
-import { StyleSheet, Text, TextInput, View } from 'react-native'
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
 import React, { useState } from 'react'
+import { useNavigation } from '@react-navigation/native';
 
 const LoginFormComponent = () => {
     const [username, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
     const [edit, setEdit] = useState<boolean>(true);
 
+    const navigate = useNavigation();
+
+    const handleSubmit = () => {
+        const userData = {
+            username: username,
+            password: password
+        }
+
+        navigate.navigate("ProfileScreen")
+    };
+
     return (
-        <View>
-            <Text style={{ paddingBottom: 25, fontSize: 35 }}>{edit ? 'Login Page' : 'Registration Page'}</Text>
+        <View style={styles.Container}>
+            <Text style={{ paddingBottom: 25, fontSize: 35, fontWeight: 'bold' }}>{edit ? 'Login Page' : 'Registration Page'}</Text>
             <TextInput
                 style={styles.Input}
                 placeholder='Username'
@@ -22,6 +34,10 @@ const LoginFormComponent = () => {
                 onChangeText={setPassword}
                 secureTextEntry
             />
+            <View style={{flexDirection: 'row', justifyContent: 'space-between', width: '75%'}}>
+                <Text style={{color: 'blue', textDecorationLine: 'underline', fontSize: 25}}>{edit ? 'Register': 'Login'}</Text>
+                <Button title='Submit' onPress={handleSubmit} />
+            </View>
         </View>
     )
 }
@@ -29,7 +45,19 @@ const LoginFormComponent = () => {
 export default LoginFormComponent
 
 const styles = StyleSheet.create({
+    Container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+    },
     Input: {
-        color: 'white'
+        width: 250,
+        height: 40,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 5,
+        backgroundColor: 'white',
+        marginBottom: 10,
+        paddingLeft: 10
     }
 })
